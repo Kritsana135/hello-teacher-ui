139ce4e1b24db73346baacd0025fef63a9a3fbf4
Kritsana135 <kritsana.syg@gmail.com>
2021-08-18 19:08:11 +0700

    update download method

--- src/screens/Certificate.tsx
+++ src/screens/Certificate.tsx
@@ -5,10 +5,9 @@ import { Button, Card, Spin } from "antd";
 import Meta from "antd/lib/card/Meta";
 import axios from "axios";
 import { FC, useEffect, useState } from "react";
+import { Document, Page, pdfjs } from "react-pdf";
 import { IForm } from "../App";
 import useLocalStorage from "../hooks/useLocalStorage";
-import { saveAs } from "file-saver";
-import { pdfjs, Document, Page } from "react-pdf";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 interface ICertificateProps {
@@ -24,30 +23,62 @@ const Certificate: FC<ICertificateProps> = ({ formState }) => {
   const [id, setId] = useLocalStorage<string | null>("id", null);
   const [pdf, setPdf] = useLocalStorage<string | null>("pdf", null);
 
-  const convertBase64ToFile = (base64String: string, fileName: string) => {
-    let arr = base64String.split(",");
-    let reg = arr[0].match(/:(.*?);/);
-    if (reg) {
-      let mime = reg[1];
-      let bstr = atob(arr[1]);
-      let n = bstr.length;
-      let uint8Array = new Uint8Array(n);
-      while (n--) {
-        uint8Array[n] = bstr.charCodeAt(n);
-      }
-      let file = new File([uint8Array], fileName, { type: mime });
-      return file;
-    }
-    return null;
-  };
+  // const convertBase64ToFile = (base64String: string, fileName: string) => {
+  //   let arr = base64String.split(",");
+  //   let reg = arr[0].match(/:(.*?);/);
+  //   if (reg) {
+  //     let mime = reg[1];
+  //     let bstr = atob(arr[1]);
+  //     let n = bstr.length;
+  //     let uint8Array = new Uint8Array(n);
+  //     while (n--) {
+  //       uint8Array[n] = bstr.charCodeAt(n);
+  //     }
+  //     let file = new File([uint8Array], fileName, { type: mime });
+  //     return file;
+  //   }
+  //   return null;
+  // };
 
-  const downloadPdf = () => {
-    const fileName = "Certificate.pdf";
-    let file = convertBase64ToFile(pdf, fileName);
-    if (file) {
-      saveAs(file, fileName);
+  // const downloadPdf = () => {
+  //   const fileName = "Certificate.pdf";
+  //   let file = convertBase64ToFile(pdf, fileName);
+  //   console.log("file", file);
+  //   if (file) {
+  //     saveAs(file, fileName);
+  //   }
+  // };
+
+  function base64toBlob(base64Data: string) {
+    const sliceSize = 1024;
+    const base64Decode = base64Data.split(",")[1];
+    const byteCharacters = atob(base64Decode);
+    const bytesLength = byteCharacters.length;
+    const slicesCount = Math.ceil(bytesLength / sliceSize);
+    const byteArrays = new Array(slicesCount);
+
+    for (let sliceIndex = 0; sliceIndex < slicesCount; ++sliceIndex) {
+      const begin = sliceIndex * sliceSize;
+      const end = Math.min(begin + sliceSize, bytesLength);
+
+      const bytes = new Array(end - begin);
+      for (let offset = begin, i = 0; offset < end; ++i, ++offset) {
+        bytes[i] = byteCharacters[offset].charCodeAt(0);
+      }
+      byteArrays[sliceIndex] = new Uint8Array(bytes);
     }
-  };
+    return new Blob(byteArrays, { type: "application/pdf" });
+  }
+
+  function openBase64NewTab(): void {
+    var blob = base64toBlob(pdf);
+    if (window.navigator && window.navigator.msSaveOrOpenBlob) {
+      window.navigator.msSaveOrOpenBlob(blob, "Certificate.pdf");
+    } else {
+      const blobUrl = URL.createObjectURL(blob);
+      window.open(blobUrl);
+    }
+  }
 
   const rePlay = () => {
     localStorage.removeItem("form");
@@ -117,7 +148,12 @@ const Certificate: FC<ICertificateProps> = ({ formState }) => {
             </div>
           }
           actions={[
-            <Button type="primary" size="large" id="play" onClick={downloadPdf}>
+            <Button
+              type="primary"
+              size="large"
+              id="play"
+              onClick={openBase64NewTab}
+            >
               ดาวน์โหลด
             </Button>,
             <Button type="default" size="large" id="play" onClick={rePlay}>
